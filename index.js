1b0357bfd59c0822b612ac4d718f87d7d3c7df14
Lukk09 <82917333+Lukk09@users.noreply.github.com>
2026-02-24 17:14:52 +0100

    Atualizar o index.js

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const titulos = document.querySelectorAll('.titulo');
 
 titulos.forEach(titulo => {
     titulo.addEventListener('click', () => {
-
         const descricao = titulo.nextElementSibling;
 
-        if (descricao.style.display === 'block') {
-            descricao.style.display = 'none';
+        if (descricao.style.maxHeight) {
+            descricao.style.maxHeight = null;
         } else {
-            descricao.style.display = 'block';
+            descricao.style.maxHeight = descricao.scrollHeight + "px"; 
         }
     });
 });
